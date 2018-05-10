dcb950a604a582b7205cc6552eff04a97664a5f7
Rob Walker <rwalker@rwalker.com>
2018-05-10 15:50:10 -0700

    deal with non-numbers, write a unified config file (hand tweaked)

--- results/results_to_json.sh
+++ results/results_to_json.sh
@@ -15,6 +15,8 @@ function results_to_json()
     declare config=
     declare examine=0
     declare extra=
+    declare NaN=
+    declare -a inputs=( )
     declare usage="
 Usage: results_to_json [OPTION]...
 
@@ -65,57 +67,59 @@ Options:
     # delete any carriage returns
     tr -d $'\r' |
         # change tabs in the file to non-whitespace to prevent IFS collapsing
-        tr $'\t' $'\1' | (
-        declare -a ofields=( )
+        tr $'\t' $'\x01' | (
         declare -a fields=( )
         declare -a values=( )
-        declare -A ofieldmap=( )
         declare -A fieldmap=( )
 
         # read first line into array "fields", note IFS only applies to read
-        IFS=$'\1' read -a ofields
+        IFS=$'\x01' read -a fields
 
-        # the field names have to be cleaned up to be json keys
-        for ((i=0; i < ${#ofields[*]}; i++))
+        # build default fieldmap, basically the field names are to
+        #  be cleaned up, and the values will be strings
+        for ((i=0; i < ${#fields[*]}; i++))
         do
-            fields[i]=${ofields[i],,}         # lower-cased
-            fields[i]=${fields[i]// /_}       # ' ' changed to '_'
-            fields[i]=${fields[i]//./}        # '.' removed
-            fields[i]=${fields[i]//$'#'/num,} # '#' changed to "num"
+            declare field=${fields[i],,} # lower-cased
+            field=${field// /_}          # ' ' changed to '_'
+            field=${field//./}           # '.' removed
+            field=${field//$'#'/num,}    # '#' changed to "num"
+
+            fieldmap[${fields[i]}]=${field}':\"%s\"'
         done
+
+        # config overrides defaults built above
+        if [[ -n ${config} ]]
+        then
+            . "${config}" || exit $?
+        fi
 
         if (( examine ))
         then
 
-            printf '################## ofieldmap ####################
-## Maps original fields to values
-declare -A ofieldmap=('
-            for ((i=0; i < ${#ofields[*]}; i++))
-            do
-                printf '[%s]="%s"
-' "${ofields[i]}" "${fields[i]}"
-            done
-
-            printf ')
-'
-
             printf '################## fieldmap ####################
-## Controls how each field is printed, as a printf format specifier.
+## Maps original fields to json fields, and how
+##  each field is printed, as a printf format specifier.
 ##  "%s" just makes a string.  If the field should be a number,
 ##  omit the quotes.  If the field should be omitted from the output
 ##  set to the empty string or remove the entry altogether
 ##
 ## TODO: more features, like True and False from zero and one?
 ##
-declare -A fieldmap=('
-            for ((i=0; i < ${#fields[*]}; i++))
+
+'
+            # equivalent to typeset -p fieldmap
+            printf 'declare -A fieldmap=(
+'
+            declare field
+            for field in "${!fieldmap[@]}"
             do
-                printf '[%s]=\\"%%s\\"
-' "${fields[i]}"
+                printf '["%s"]=%s
+' "${field}" "${fieldmap[${field}]}"
             done
 
             printf ')
 '
+
             printf '################## sample json ####################
 ## how the data would look by default
 '
@@ -124,15 +128,28 @@ declare -A fieldmap=('
                 declare record=
                 [[ -n ${extra} ]] && record+='   '"${extra}"'
 '
-                IFS=$'\1' read -a values
+                IFS=$'\x01' read -a values
                 # echo ${#fields[*]} fields, ${#values[*]} values
                 # prepend "extra" to each record
                 for ((i=0; i < ${#fields[*]}; i++))
                 do
-                    declare field=${fields[i]}
-                    declare value=
+                    declare mapentry=${fieldmap[${fields[i]}]}
+                    [[ -z ${mapentry} ]] && continue; # skip if absent
 
-                    printf -v value "${fieldmap[${field}]}" "${values[i]}" || exit $?
+                    declare field=${mapentry%:*}
+                    declare format=${mapentry#*:}
+                    declare value=${values[i]}
+
+                    if [[ ${format} == '%d' || ${format} == '%f' ]]
+                    then
+                        value=${value//$'['/}
+                        value=${value//$']'/}
+                        value=${value#+*}
+
+                        [[ ! ${values[i]} =~ -?[0-9]+(.[0-9]+)? ]] && value=
+                    fi
+
+                    printf -v value "${format}" "${value}" || exit $?
 
                     record+='   "'"${field}"'":'"${value}"',
 '
@@ -145,26 +162,36 @@ declare -A fieldmap=('
             done
 
         else
-            if [[ -n ${config} ]]
-            then
-                . "${config}" || exit $?
-            fi
-
-            # read subsequent lines into array "fields"
-            while IFS=$'\1' read -a values
+            # read subsequent lines into array values
+            while IFS=$'\x01' read -a values
             do
                 declare record=
-                # echo ${#fields[*]} fields, ${#values[*]} values
-                # prepend "extra" to each record
+
                 for ((i=0; i < ${#fields[*]}; i++))
                 do
-                    declare field=${fields[i]}
-                    declare value=
+                    declare mapentry=${fieldmap[${fields[i]}]}
+                    [[ -z ${mapentry} ]] && continue; # skip if absent
 
-                    printf -v value "${fieldmap[${field}]}" "${values[i]}" || exit $?
+                    declare field=${mapentry%:*}
+                    declare format=${mapentry#*:}
+                    declare value=${values[i]}
+
+                    # cleanup for numbers
+                    if [[ ${format} == '%d' || ${format} == '%f' ]]
+                    then
+                        value=${value//$'['/}
+                        value=${value//$']'/}
+                        value=${value#+*}
+
+                        [[ ! ${values[i]} =~ -?[0-9]+(.[0-9]+)? ]] && value=
+                    fi
+
+                    printf -v value "${format}" "${value}" || exit $?
 
                     record+='"'"${field}"'":'"${value}"','
                 done
+
+                # prepend "extra" to each record
                 printf "{%s%s}\n" "${extra}" "${record%,}"
             done
         fi
