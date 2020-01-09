719ce0da36f6b89f73637304e5aedade094f399d
Rob Walker <rwalker@rwalker.com>
2020-01-09 15:49:29 -0800

    easier

--- results/query.sh
+++ results/query.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 INPUTRC=query.inputrc
-while read -e -r -p "last_name [first_name]: " last_name first_name rest;
+while read -e -r -p "name: " first_name last_name rest;
 do
   last_name=${last_name^^}
   first_name=${first_name^^}
-  if [[ -n ${first_name} ]]
+  if [[ -n ${last_name} ]]
   then
       jq -Mc '. | select((.last_name | ascii_upcase)=="'"${last_name}"'") | select((.first_name | ascii_upcase)=="'"${first_name}"'")' < results.json
   else
-      jq -Mc '. | select((.last_name | ascii_upcase)=="'"${last_name}"'")' < results.json
+      jq -Mc '. | select((.last_name | ascii_upcase)=="'"${first_name}"'")' < results.json
   fi
 
 done
